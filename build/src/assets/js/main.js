f3833e5c258fff1ec5f120da6589df08c24ce1e9
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-03-13 00:38:45 +0100

    remove package json

---
+++ build/src/assets/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Load head content
+  fetch('../../partials/mainhead.html').then(response => response.text()).then(html => {
+    document.head.innerHTML += html;
+  }).catch(error => {
+    console.error('Error loading head partial:', error);
+  });
+
+  // Load footer content
+  // fetch('/partials/footer.html')
+  //   .then(response => response.text())
+  //   .then(html => {
+  //     document.getElementById('footer-container').innerHTML = html;
+  //   })
+  //   .catch(error => {
+  //     console.error('Error loading footer partial:', error);
+  //   });
+});
